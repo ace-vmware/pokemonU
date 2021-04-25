1239899196c7a95967c49cc7be382bddcb43234c
Adam Evancho <aevancho@aevancho-a02.vmware.com>
2021-04-24 21:19:34 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import { Pokedex } from 'pokeapi-js-wrapper'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customOptions = {
+  protocol: "https",
+  hostName: "pokeapi.co/",
+  versionPath: "/api/v2/",
+  cache: true,
+  timeout: 5 * 1000, // 5s
+  cacheImages: true
+}
+const P = new Pokedex(customOptions)
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: []
+    }
+  }
+
+  async componentDidMount() {
+    const kantoPokedex = await P.getPokedexByName("kanto")
+    this.setState({
+      pokemon: [kantoPokedex.pokemon_entries]
+    })
+  }
+
+  async fetchPokemonByName(name) {
+    const fetchedPokemon = await P.getPokemonByName(name)
+    console.log(fetchedPokemon)
+  }
+
+  
+
+  render() {
+      const allImages = this.state.pokemon.map((pokemon) => {
+        const pokes = pokemon.map((poke) => {
+          console.log(poke)
+          const id = poke.entry_number
+          const name = poke.pokemon_species.name
+          const src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+          // return <img src={src} alt="poke"/>
+          return <p>{id}. {name}</p>
+        })
+        return pokes
+      })
+    return(
+      <div>
+        <p>Words</p>
+        <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"
+        alt="Pikachu"
+      />
+      {allImages}
+      </div>
+    )
+  }
 }
 
 export default App;
